76b490fa22dcd863d11d93638cb4338a95a2dc6c
Boui01 <168456269+Boui01@users.noreply.github.com>
2024-09-04 23:05:09 +0700

    Update DetectText.js

--- DetectText.js
+++ DetectText.js
@@ -1,5 +1,5 @@
 /* 
-Detect word or sentent in Text you in put such as "hello world" check "hello" or "lo" output is true
+Detect word or sentence in Text you in put such as "hello world" check "hello" or "lo" output is true
 Type
     1 ===  word
     2 ===  sentence
@@ -7,7 +7,7 @@ Type
 function DetectText (text , detect , type){
     let result = false;
 
-    if(type === 1){
+    if(type === 1 || type === 'word'){
         for(let position = 0; position < text.length ; position++){
             if (text[position] === detect) {
                 console.warn('Text detected.',text)
@@ -17,7 +17,7 @@ function DetectText (text , detect , type){
         }
         console.warn('The text parameter must be a string.',text)
     }
-    else if(type === 2){
+    else if(type === 2 || type === 'sentence'){
         if( text.includes(detect)){
             result = true;
         }
