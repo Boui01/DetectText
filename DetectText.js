8ea47879c26aeeb5256e27b654e9de79345d5951
Boui01 <168456269+Boui01@users.noreply.github.com>
2024-09-04 22:56:05 +0700

    This file function DetectText for import .
    You can use for check text word or sentence and output true or false.

---
+++ DetectText.js
@@ -0,0 +1,29 @@
+/* 
+Detect word or sentent in Text you in put such as "hello world" check "hello" or "lo" output is true
+Type
+    1 ===  word
+    2 ===  sentence
+*/
+function DetectText (text , detect , type){
+    let result = false;
+
+    if(type === 1){
+        for(let position = 0; position < text.length ; position++){
+            if (text[position] === detect) {
+                console.warn('Text detected.',text)
+                result = true;
+                break;
+            }
+        }
+        console.warn('The text parameter must be a string.',text)
+    }
+    else if(type === 2){
+        if( text.includes(detect)){
+            result = true;
+        }
+    }
+
+    return result
+}
+
+export default DetectText
